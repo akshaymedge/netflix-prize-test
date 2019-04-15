26acfeedcc765cc9ef861ea23bd9fced3de4b0ae
akshaymedge <akshaymedge@gmail.com>
2019-04-15 16:16:39 -0400

    Rel 1:Prediction & RMSE caclulations updated.
    Signed-off-by: akshaymedge <akshaymedge@gmail.com>

--- movie_movie_similarity_calculation.py
+++ movie_movie_similarity_calculation.py
@@ -1,68 +1,89 @@
-# from math import sqrt
 from scipy import sparse
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-# from sklearn.metrics import mean_squared_error
 from datetime import datetime
 import os
 from rmse_calculator import calculate_rmse
-
-system_flag = 0
-
-DATA_PATH = 'C:/Users/amedge/Downloads/netflix-prize-data/{}' if system_flag == 1 else 'C:/Users/Akshay Medge/' \
-                                                                                       'Downloads/netflix-prize-data/{}'
-FILE_PATH = 'C:/Users/amedge/Desktop/Project/netflix-prize-test/combined_data.csv' if system_flag == 1 else 'C:/Users/Akshay Medge/Desktop/Github/ALDA/netflix-prize-test/combined_data.csv'
+import numpy as np
 
 
-def predict(user_id, movies_list, movie_user_dictionary, similarity_list, ideal_k = 30):
-    neighbor_count, similarity_rating_sum, similarity_sum = 0
+system_flag = 1
+
+DATA_PATH = 'C:/Users/amedge/Downloads/netflix-prize-data/{}' if system_flag == 1 else\
+    'C:/Users/Akshay Medge/Downloads/netflix-prize-data/{}'
+FILE_PATH = 'C:/Users/amedge/Desktop/Project/netflix-prize-test/combined_data.csv' if system_flag == 1 else\
+    'C:/Users/Akshay Medge/Desktop/Github/ALDA/netflix-prize-test/combined_data.csv'
+
+
+def predict(user_id, movies_list, movie_user_dictionary, similarity_list, ideal_k=30):
+    logs_file = open("Prediction_logs.txt", "w")
+    logs_file.write("User: {}\n".format(user_id))
+    neighbor_count, similarity_rating_sum, similarity_sum = 0, 0, 0
     for movie in movies_list:
-        if neighbor_count < ideal_k and user_id in movie_user_dictionary[movie].keys():
-            neighbor_count += 1
-            movie_similarity = similarity_list[movies_list.index(movie)]
-            similarity_rating_sum += (movie_user_dictionary[movie][user_id] * movie_similarity)
-            similarity_sum += movie_similarity
-
+        if movie != 0:
+            # print("User is of type: {}".format(type(user_id)))
+            logs_file.write("Movie: {}\n".format(movie))
+            logs_file.write("Users who have rated the movie: {}\n".format(list(movie_user_dictionary[movie].keys())))
+            if neighbor_count < ideal_k and int(user_id) in list(movie_user_dictionary[movie].keys()):
+                neighbor_count += 1
+                mov_idx = np.where(movies_list == movie)
+                movie_similarity = similarity_list[int(mov_idx[0])]  # movies_list.index(movie)]
+                # print("M_sim: {}".format(movie_similarity))
+                # print("MUD: {}".format(movie_user_dictionary[movie]))
+                similarity_rating_sum += (movie_user_dictionary[movie][int(user_id)] * movie_similarity)
+                # print("Sim_Rating_sum: {}".format(similarity_rating_sum))
+                similarity_sum += movie_similarity
+    logs_file.close()
     return similarity_rating_sum / similarity_sum
 
 
-def predict_rating_for(movie, user, similar_movies, movie_users_dictionary):
+def predict_rating_for(user, similar_movies, m_u_dictionary):
     sorted_similarities = sorted(similar_movies, reverse=True)
     sorted_similar_movie_ids = similar_movies.argsort()[::-1][1:]
 
-    print("Similarities in a sorted order: {}".format(sorted_similarities))
-    print("Similar Movies in sorted order: {}".format(sorted_similar_movie_ids))
+    # print("Similarities in a sorted order: {}".format(sorted_similarities))
+    # print("Similar Movies in sorted order: {}".format(sorted_similar_movie_ids))
 
-    return predict(user, sorted_similar_movie_ids, movie_users_dictionary, sorted_similarities)
+    return predict(user, sorted_similar_movie_ids, m_u_dictionary, sorted_similarities)
 
 
 def movie_user_rating_mapping(list_file):
     ratings_tmp = {}
-
+    print("Starting to compute Movie --> Users:Rating Mappings...")
     mov_r = {}
     for file in list_file:
         start_file_read = datetime.now()
         with open(DATA_PATH.format(file)) as test:
-            for line in test:
-                if line[-2] == ":":
-                    j = int(line[0:len(line) - 2])
+            for data_line in test:
+                if data_line[-2] == ":":
+                    j = int(data_line[0:len(data_line) - 2])
                     ratings_tmp = {}
                 else:
-                    ratings_tmp.update({int(line.split(",")[0]): int(line.split(",")[1])})
+                    ratings_tmp.update({int(data_line.split(",")[0]): int(data_line.split(",")[1])})
                     mov_r.update({j: ratings_tmp})
         end_file_read = datetime.now()
-        print('done with {} file in {}'.format(file, (end_file_read - start_file_read)))
-    # print("For Movie: 3455 ---> {}".format(mov_r[3455]))
+        print('Done with {} file in {}'.format(file, (end_file_read - start_file_read)))
+    print("Mappings computed.\n")
     return mov_r
 
 
+def similarity_mapping(param):
+    similarity_dictionary = {}
+    for movie_idx in range(0, len(param)):
+        similarity_dictionary[movie_idx] = param[movie_idx]
+    print("Similarity Mapping Dictionary: {}".format(similarity_dictionary))
+    return similarity_mapping()
+
+
+print("\n")
+program_time_start = datetime.now()
 if not os.path.isfile("all_data_sparse_matrix.npz"):
     print("Loading all data....")
     start_load = datetime.now()
     combined_data_df = pd.read_csv(FILE_PATH, names=[
         'movie', 'user', 'rating', 'date'], sep=",")
     end_load = datetime.now()
-    print("Loading complete in: {}".format(end_load - start_load))
+    print("Loading complete in: {}\n".format(end_load - start_load))
 
     print("Creating Sparse Matrix....")
     start_sparse = datetime.now()
@@ -86,46 +107,62 @@ elif not os.path.isfile("movie_movie_similarity.npz"):
 
 print("Loading Similarity Matrix...")
 m_m_sim_matrix = sparse.load_npz("movie_movie_similarity.npz")
-print("Movie Movie similarity matrix has a shape: {}".format(m_m_sim_matrix.shape))
+print("Movie Movie similarity matrix has a shape: {}\n".format(m_m_sim_matrix.shape))
 
 start_dict_creation = datetime.now()
 movie_users_dictionary = movie_user_rating_mapping(['combined_data_1.txt', 'combined_data_2.txt', 'combined_data_3.txt',
-                                        'combined_data_4.txt'])
+                                                    'combined_data_4.txt'])
 end_dict_creation = datetime.now()
-print("Time for creating entire dictionary: {}".format(end_dict_creation - start_dict_creation))
+print("Time for creating entire dictionary: {}\n".format(end_dict_creation - start_dict_creation))
 
 
 # Working on actual test data:
+print("Prediction for qualifying.txt in progress...")
+qualifying_prediction_start = datetime.now()
 with open(DATA_PATH.format('qualifying.txt')) as test_data:
     output_file = open("Qualifying_Predictions.txt", "w")
     movie_id = 0
-    for line in test_data:
-        if ":" in line:
-            movie_id = line.replace(":", "")
+    for test_line in test_data:
+        if ":" in test_line:
+            movie_id = test_line.replace(":", "")
             output_file.write("For Movie: {}".format(movie_id))
         else:
-            prediction = predict_rating_for(movie_id, line.split(',')[0], m_m_sim_matrix[movie_id].toarray().ravel(),
-                               movie_users_dictionary)
-            output_file.write("{}:{}".format(line.split(',')[0], prediction))
+            user = test_line.split(',')[0]
+            prediction = predict_rating_for(user, m_m_sim_matrix[movie_id].toarray().ravel(),
+                                            movie_users_dictionary)
+            print("Rating for the above movie by User: {} is: {}".format(user, prediction))
+            output_file.write("{}:{}".format(test_line.split(',')[0], prediction))
     output_file.close()
+qualifying_prediction_end = datetime.now()
+print("Prediction for qualifying.txt completed. Time taken: {}\n".format(qualifying_prediction_end -
+                                                                         qualifying_prediction_start))
 
 # Working on Probe data:
 probe_actual_output = []
+probe_prediction_start = datetime.now()
 with open(DATA_PATH.format('probe.txt')) as test_data:
     output_file = open("Probe_Predictions.txt", "w")
     movie_id = 0
-    for line in test_data:
-        if ":" in line:
-            movie_id = line.replace(":", "")
+    for probe_line in test_data:
+        if ":" in probe_line:
+            movie_id = probe_line.replace(":", "")
             output_file.write("For Movie: {}".format(movie_id))
         else:
             # Actual Value for RMSE
-            probe_actual_output.append(movie_users_dictionary[movie_id][line.split(',')[0]])
+            user = probe_line.split(',')[0]
+            probe_actual_output.append(movie_users_dictionary[movie_id][int(probe_line.split(',')[0])])
             # For Prediction
-            prediction = predict_rating_for(movie_id, line.split(',')[0], m_m_sim_matrix[movie_id].toarray().ravel(),
-                               movie_users_dictionary)
-            output_file.write("{}:{}".format(line.split(',')[0], prediction))
+            prediction = predict_rating_for(probe_line.split(',')[0], m_m_sim_matrix[movie_id].toarray().ravel(),
+                                            movie_users_dictionary)
+            print("Rating for the above movie in Probe by User: {} is: {}".format(user, prediction))
+            output_file.write("{}:{}".format(probe_line.split(',')[0], round(prediction)))
     output_file.close()
+probe_prediction_end = datetime.now()
+print("Prediction for probe.txt completed. Time taken: {}\n".format(probe_prediction_end -
+                                                                    probe_prediction_start))
 
 
-print("I really hope this prints out. If it does, RMSE will be: {}".format(calculate_rmse(probe_actual_output)))
+print("I really hope this prints out. If it does, RMSE will be: {}\n".format(calculate_rmse(probe_actual_output)))
+program_time_end = datetime.now()
+
+print("Congratulations Son. Program completed in {}".format(program_time_end - program_time_start))
--- rmse_calculator.py
+++ rmse_calculator.py
@@ -1,13 +1,14 @@
 import os
 from math import sqrt
-import movie_movie_similarity_calculation as movsc
+# import movie_movie_similarity_calculation as movsc
 from sklearn.metrics import mean_squared_error
 
+DATA_PATH = 'C:/Users/amedge/Downloads/netflix-prize-data/{}'
 
 def calculate_rmse(actual_output):
     predictions = []
     if os.path.isfile("Probe_Predictions.txt"):
-        with open(movsc.FILE_PATH.format("Probe_Predictions.txt")) as probe_pred:
+        with open(DATA_PATH.format("Probe_Predictions.txt")) as probe_pred:
             for line in probe_pred:
                 predictions.append(line.split(":")[1])
 
